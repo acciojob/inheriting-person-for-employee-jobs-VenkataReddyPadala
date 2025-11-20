d5c849467581d186d4a51eefdbe85c9094023d93
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-20 16:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-// complete this js code
-class Person{
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
 	}
-	greet(){
-		return `Hello, my name is ${this.name}, I am ${this.age} years old.`
+
+	greet() {
+		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 	}
 }
-class Employee extends Person{
-	constructor(name,age,jobTitle){
-		super(name,age);
-		this.jobTitle=jobTitle;
+
+class Employee extends Person {
+	constructor(name, age, jobTitle) {
+		super(name, age);
+		this.jobTitle = jobTitle;
 	}
-	jobGreet(){
-		return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+
+	jobGreet() {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
 
